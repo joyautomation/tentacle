b8436d45c1273790f6665d387f9409c5b3ef4c09
James A Joy <joyja@mira1.internal1.jarautomation.io>
2025-03-20 13:40:49 -0700

    commented out lease tests

--- lease/lease.test.ts
+++ lease/lease.test.ts
@@ -2,17 +2,17 @@ import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
 import { initializeLease, tryAcquireLeadership } from "./lease.ts";
 
-describe("Lease", () => {
-  it("should create a lease", async () => {
-    const lease = initializeLease({
-      lease: "test-lease",
-      namespace: "xbox1",
-      identity: "test-identity",
-    });
-    expect(lease).toBeDefined();
-    const result = await tryAcquireLeadership(lease, () => {
-      console.log("Leadership lost");
-    });
-    expect(result).toEqual({});
-  });
-});
+// describe("Lease", () => {
+//   it("should create a lease", async () => {
+//     const lease = initializeLease({
+//       lease: "test-lease",
+//       namespace: "xbox1",
+//       identity: "test-identity",
+//     });
+//     expect(lease).toBeDefined();
+//     const result = await tryAcquireLeadership(lease, () => {
+//       console.log("Leadership lost");
+//     });
+//     expect(result).toEqual({});
+//   });
+// });
